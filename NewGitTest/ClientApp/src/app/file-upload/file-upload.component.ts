71882ce9078431d2e77ede885ef970ede535c5e4
zahilkuttathil <zahilkuttathil@hotmail.com>
2021-09-25 22:33:44 -0400

    added upload app folder for angular

---
+++ NewGitTest/ClientApp/src/app/file-upload/file-upload.component.ts
@@ -0,0 +1,26 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-file-upload',
+  templateUrl: './file-upload.component.html'
+})
+export class Upload {
+  $key: string;
+  file: File;
+  name: string;
+  url: string;
+  progress: number;
+  createdAt: Date = new Date();
+
+  constructor(file: File) {
+    this.file = file;
+  }
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
